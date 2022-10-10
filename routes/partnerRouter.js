94736af2f2573ec2041e1025d34ac5bcb783a7cf
BenPavlovich <Pavlovichben@gmail.com>
2022-10-09 18:39:04 -0700

    Week 2 Submission

---
+++ routes/partnerRouter.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const Partner = require('../models/partner');
+const partnersRouter = express.Router();
+
+partnersRouter.route('/')
+.get((req, res, next) => {
+    Partner.find()
+    .then(partners => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partners);
+    })
+.catch(err => next(err))
+})
+
+.post((req, res, next) => {
+    Partner.create(req.body)
+    .then(partner => {
+        console.log('Partner Created', partner);
+        res.statusCode=200;
+        res.setHeader('Content-Type', 'application/json')
+        res.json(partner)
+    })
+.catch(err => next(err));
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partners');
+})
+.delete((req, res, next) => {
+    Partner.deleteMany()
+    .then(response =>{
+     res.statusCode=200;
+     res.setHeader('Content-Type', 'application/json');
+     res.json(response);
+    })
+    .catch(err => next(err));
+ });
+
+partnersRouter.route('/:partnerId')
+.get((req, res, next) => {
+    Partner.findById(req.params.partnerId)
+    .then(partner => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(partner);
+    
+    })
+    .catch(err => next(err));
+})
+.post((req, res, next) => {
+    Partner.create(req.body)
+    .then(partner => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
+    })
+   .catch(err => next(err))
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('Cant use put operations in partner')
+})
+.delete((req, res, next) => {
+    Partner.findByIdAndDelete(req.params.partnerId)
+    .then(partner => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
+})
+.catch(err => next(err));
+})
+module.exports = partnersRouter;
