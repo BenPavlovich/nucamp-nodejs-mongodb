2d4f26a872085f1dda7dc32bf1e3b3fc714e460b
Ben Pavlovich <Pavlovichben@gmail.com>
2022-10-24 09:43:34 -0700

    Week 4 update

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('passport');
+const config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const partnerRouter = require('./routes/partnerRouter')
 const promotionRouter = require('./routes/promotionRouter');
+const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
+
 
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost:27017/nucampsite';
+const sessionFileStore = require('session-file-store');
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
   useFindAndModify: false,
@@ -24,6 +29,16 @@ err => console.log(err)
 );
 var app = express();
 
+app.all("*", (req, res, next) => {
+  if (req.secure) {
+    return next();
+  
+  }else{
+    console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+    res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`)
+  }
+})
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -31,14 +46,25 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cookieParser('12345-6780-09876-54321'));
+
+
+
+app.use(passport.initialize());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter)
 app.use('/partners', partnerRouter)
+app.use('/imageUpload', uploadRouter)
+app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
